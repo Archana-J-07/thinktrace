eb2fadee3527e286a119625512233cb19502da66
Archana-J-07 <anajoshen007@gmail.com>
2026-09-16 09:23:21 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 import App from "./App";
+import { trpc } from "./lib/trpc";
 import "./index.css";
+
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "/api/trpc",
+    }),
+  ],
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <App />
+      </trpc.Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
